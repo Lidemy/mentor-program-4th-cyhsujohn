0cadbd0af005b9c70a600f2008a5e0320374ebfa
ajohn <cyhsu.john@gmail.com>
2020-07-06 22:37:37 +0800

    wk2 completed

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,19 @@
 function reverse(str) {
-  
+  let reversedStr = []
+  for (let i=str.length-1; i>=0; i--) {
+  	reversedStr.push(str[i])
+  }
+  console.log(reversedStr.join(''))
 }
+
+
+/* 看完範例後更直覺的解法
+function reverse(str) {
+	let reversedStr = ''
+	for (let i=str.length-1; i>=0; i--) {
+	reversedStr + str[i]
+	}
+	console.log(reversedStr)
+} */
 
 reverse('hello');
