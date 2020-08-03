8f0d153fb70d11d32fe154afc677a390b538170e
ajohn <cyhsu.john@gmail.com>
2020-08-04 02:36:10 +0800

    all done

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -3,9 +3,9 @@ const request = require('request');
 request(
   'https://lidemy-book-store.herokuapp.com/books?_limit=10',
   (error, response, body) => {
-    const json = JSON.parse(body);
-    for (let i = 0; i < json.length; i += 1) {
-      console.log(`${json[i].id} ${json[i].name}`);
+    const data = JSON.parse(body);
+    for (let i = 0; i < data.length; i += 1) {
+      console.log(`${data[i].id} ${data[i].name}`);
     }
   },
 );
